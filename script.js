e518d8ea33dd609364716677a9a84822f6b871e6
romancgilbert <roman.c.gilbert@gmail.com>
2025-10-24 00:01:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple contact form (demo)
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thanks for reaching out! I’ll get back to you soon.');
+  this.reset();
+});
