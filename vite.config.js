4625e40c64e82a983e793005e35d53ea93fdf0c1
romancgilbert <roman.c.gilbert@gmail.com>
2025-10-29 13:24:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-photo-portfolio/', // 👈 use your repo name here
+  base: '/my-photography-portfolio/', // 👈 use your repo name here
 })
